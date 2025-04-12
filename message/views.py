ff5859e3f5cbb936484bb6dd252a9f39e78bf1c2
amir <amahmoudian21@gmail.com>
2025-04-12 09:20:56 -0400

    message board is ready.

--- message/views.py
+++ message/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
-
+from django.views.generic import ListView
+from .models import Message
 
 # def MessageView(request):
 #     return render (request,'home.html')
 
-class MessageView(TemplateView):
+class MessageView(ListView):
+    model = Message
     template_name = 'home.html'
